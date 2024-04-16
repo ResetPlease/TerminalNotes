ce7b76c8e6e1dfcb3f33965ee2ed9392734235b4
ResetPlease <unusovr2004@gmail.com>
2024-04-16 13:33:43 +0300

    Изменить логику хранения данных

--- todoapp.cpp
+++ todoapp.cpp
@@ -1,33 +1,28 @@
 // coded by iveseentheworld
 
-#include <filesystem>
 #include <stdio.h>
-#include <vector>
+#include <string.h>
 #include <string>
 #include <time.h>
-#include <string.h>
+#include <vector>
 
-#define PATH_MAX_SIZE 1024
+
 #define DATA_MAX_SIZE 512
 
 
-const char filename[] = "/home/beregom/Projects/ITODO/todoapp.data";
-const char logo_filename[] = "/home/beregom/Projects/ITODO/logo.txt";
+const char filename[] = ".todoapp.data";
 const char RESET[] = "\u001b[0m";
 const char GREEN[] = "\u001b[32m";
-
 
 
 struct Comment
 {
 
     char clock[1024];
-    char path[PATH_MAX_SIZE];
     char data[DATA_MAX_SIZE];
 
-    Comment(const std::string& first, const std::string& second){
-        strcpy(path, first.c_str());
-        strcpy(data, second.c_str());
+    Comment(const char* second){
+        strcpy(data, second);
         time_t t = time(NULL);
         tm *tm = localtime(&t);
         strcpy(clock, asctime(tm));
@@ -38,33 +33,26 @@ struct Comment
 };
 
 
-void print_logo(){
-    FILE* file = fopen(logo_filename, "rb");
-    if(file == NULL){
-        printf("ERROR: Logo file cannot be read :( \n");
-        return;
-    }
-    char c;
-    printf(GREEN);
-    while(fread(&c, sizeof(char), 1, file)){
-        printf("%c", c);
-    }
-    printf("\n");
-    printf(RESET);
+void print_help(){
+    printf("It is Note app for different folders\n");
+    printf("Use:\n\t+ \"comment\" for set note in this folder\n\t- <number> for delete note \
+with this number. Numbering starts from 0\n\tclear - clearing all notes in a given folder\n");
+    printf("\n(c) iveseentheworld\n");
 }
 
-void print_help(){
-    print_logo();
-    printf("It is TODO app for different folders\n");
-    printf("INFO: DELETE IS NOT IMPLEMENTED\n");
-    printf("Use:\n\t+ \"comment\" for set todo in this folder\n\t- <number> for delete todo with this number\n");
-}
 
 void print_comment(const Comment& obj){
     static size_t index_of_note = 0;
     printf("%s%s%s %lu:- %s\n\n", GREEN, obj.clock, RESET, index_of_note++, obj.data);
 }
 
+void data_remove(){
+    if(remove(filename) == 0){
+        printf("INFO: all data was successfully deleted\n");
+    }else{
+        printf("INFO: An error occurred during deletion. Perhaps the note was not initialized here\n");
+    }
+}
 
 
 int main(int argc, char** argv){
@@ -74,20 +62,16 @@ int main(int argc, char** argv){
         return 0;
     }
     
-    std::string path = std::filesystem::current_path();
-    printf("Current directory: %s\n\n", path.c_str());
-
     if(argc == 1){
         FILE* file = fopen(filename, "rb");
         if(file == NULL){
-            printf("ERROR: %s file cannot be read\n", filename);
+            printf("No records for this directory\n");
+            printf("Use -h for more information\n");
             return 0;
         }
         Comment data;
         while(fread(&data, sizeof(Comment), 1, file)){
-            if( std::string(data.path) == path ){
-                print_comment(data);
-            }
+            print_comment(data);
         }
         fclose(file);
         return 0;
@@ -95,45 +79,40 @@ int main(int argc, char** argv){
 
     std::string option = std::string(argv[1]);
     
-    if(option != std::string("+") && option != std::string("-")){
+    if(option != std::string("+") && option != std::string("-") && option != std::string("clear")){
         printf("ERROR: Choose the correct option\n");
         print_help();
-        return 0;
-    }
-    
-    if(path.size() >= PATH_MAX_SIZE){
-        printf("ERROR: file path too long %s\n", path.c_str());
         return 0;
     }
     
     if(option == "+"){
         std::string data = std::string(argv[2]);
         if(data.size() >= DATA_MAX_SIZE){
-            printf("ERROR: comment is too big\n");
+            printf("ERROR: Note is too big\n");
             return 0;
         }
-        Comment obj(path.c_str(), data.c_str());
-        FILE* file = fopen(filename, "ab"); // дописываем информацию к файлу
+        Comment obj(data.c_str());
+        FILE* file = fopen(filename, "ab");
         if(file == NULL){
-            printf("ERROR: can't write in this file\n.");
-            printf("INFO: Crating a new file with \"wb\" mode %s\n", filename);
+            printf("INFO: Creating a new file with \"wb\" mode %s\n", filename);
             file = fopen(filename, "wb");
         }
         fwrite(&obj, sizeof(Comment), 1, file);
         fclose(file);
-        printf("Comment added successfully\n");
+        printf("Note added successfully\n");
     }else if(option == "-"){
         long long int number;
         try{
             number = std::stoll(std::string(argv[2]));
         }catch(...){
-            printf("ERROR: Please enter the comment line number\n");
+            printf("ERROR: Please enter the note line number\n");
             return 0;
         }
         if(number < 0){
             printf("ERROR: Line number can't be negative\n");
             return 0;    
         }
+
         FILE* file = fopen(filename, "rb");
         if(file == NULL){
             printf("ERROR: %s file cannot be read\n", filename);
@@ -144,32 +123,12 @@ int main(int argc, char** argv){
         while(fread(&obj, sizeof(Comment), 1, file)){
             data.push_back(obj);
         }
+        fclose(file);
 
         if(number >= (long long)data.size()){
             printf("ERROR: Wrong number of line (too big)\n");
             return 0;
-        }
-
-        size_t number_of_notes = 0;
-        bool been_deleted = false;
-        std::vector<Comment> without_number;
-        for(size_t i = 0; i < data.size(); ++i){
-            if(std::string(data[i].path) == path){
-                if(number_of_notes == (size_t)number){
-                    number_of_notes++;
-                    been_deleted = true;
-                    continue;
-                }
-                number_of_notes++;
-            }
-            without_number.push_back(data[i]);
-        }
-        fclose(file);
-
-        if(!been_deleted){
-            printf("ERROR: Wrong number. There are no comments with this number\n");
-            return 0;
-        }
+        }    
 
         file = fopen(filename, "wb");
         if(file == NULL){
@@ -177,16 +136,19 @@ int main(int argc, char** argv){
             return 0;
         }
         size_t written = 0;
-        for(size_t i = 0; i < without_number.size(); ++i){
-            written = fwrite(&without_number[i], sizeof(Comment), 1, file);
-            if(written == 0){
-                printf("ERROR: rewrite failed\n");
-                fclose(file);
-                return 0;
+        for(size_t i = 0; i < data.size(); ++i){
+            if(i != number){
+                written = fwrite(&data[i], sizeof(Comment), 1, file);
+                if(written == 0){
+                    printf("ERROR: rewrite failed\n");
+                    fclose(file);
+                    return 0;
+                }
             }
         }
         fclose(file);
-        printf("Comment successfully deleted\n");
+
+        printf("Note successfully deleted\n");
     }
     return 0;
 }
